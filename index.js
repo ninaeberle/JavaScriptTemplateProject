f33db8555de28c3d1762de48bf8bade3a0ce5a50
nina eberle <eberle@punkt.de>
2020-03-25 17:25:41 +0100

    elements improved

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
+//Text changes by clicking it and sale code appears
+    function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+    }
+    function changeText(id) {
+    id.innerHTML = "Use code 1234 to get 20% off your first purchase!";
+    }
 
 
+
+//input boy fpr registration 
+//need to still edit the color when input 
+//need to still add some more input elements like password
+    function myFunction(x) {
+    var x = document.getElementById("fname");
+    x.style.background = "yellow";
+    }
+    function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+    }
+    function myFunctions() {
+    var x = document.getElementById("e-mail");
+    }
+
+
+
+//register button changes Text by click
+    function mDown(obj) {
+        obj.style.backgroundColor = "rgba(0,0,0,0.6)";
+    }
+    
+    function mUp(obj) {
+        obj.style.backgroundColor="#261300";
+        obj.innerHTML="Thank You!";
+    }
+
+
+
+
+
+    
